8ceaf740f76183fbbd423bac2061f19f5e6098c9
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2023-01-15 15:38:42 -0500

    check log

--- frontend/src/components/Utills/GetDate.js
+++ frontend/src/components/Utills/GetDate.js
@@ -27,6 +27,7 @@ const getDate = (date) => {
     const month = date.getMonth();
     const day = date.getDate();
     const year = date.getFullYear();
+    console.log("month, day, year:", month, day, year)
 
     let result = `${months[month]} ${day} ${year}`;
 
